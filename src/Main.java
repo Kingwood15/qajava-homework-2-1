0d04f537e713c4f62f7af7b402eb944c762a732c
Азат Ильдарханов <defcon797@gmail.com>
2022-04-22 21:10:43 +0300

    Add Main.java and .gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 190;
+        int bonusPerMile = 20;
+        int bonusMiles = ticketPrice / bonusPerMile;
+
+        System.out.println("За билет Вы получите " + bonusMiles + " бонусных миль.");
+    }
+}
